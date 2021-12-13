d6e2bb55e3a84dc7eaaef79ad88683f1fde2fe13
kalmanbendeguz <61018403+kalmanbendeguz@users.noreply.github.com>
2021-12-13 01:11:09 +0100

    inventory name

--- app/src/main/java/kb/inventory/MainActivity.kt
+++ app/src/main/java/kb/inventory/MainActivity.kt
@@ -1,26 +1,31 @@
 package kb.inventory
 
 import android.content.Context
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
+import android.text.InputType
 import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.android.volley.*
+import com.android.volley.toolbox.HttpHeaderParser
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.google.android.material.navigation.NavigationView
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import kb.inventory.qbarcode.Capture
+import org.json.JSONException
+import org.json.JSONObject
+import java.io.UnsupportedEncodingException
+
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -87,30 +92,138 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             intentIntegrator.initiateScan()
 
         }
+
+        val tvInventoryName: TextView = findViewById(R.id.tvInventoryName)
+        val queue = Volley.newRequestQueue(this)
+        val inventoryNameURL = "http://$currentServerIP:$currentPort/inventory/get_name"
+
+        val stringRequest = StringRequest(
+            Request.Method.GET, inventoryNameURL,
+            { response ->
+                if (response != "") {
+                    tvInventoryName.text = "Raktár neve: " + response.toString()
+                } else {
+
+                    val nameInventoryDialog = AlertDialog.Builder(this@MainActivity)
+                    nameInventoryDialog.setTitle("Adj nevet a raktárnak!")
+
+                    val inventoryNameInput = EditText(this@MainActivity)
+                    inventoryNameInput.inputType = InputType.TYPE_CLASS_TEXT
+                    inventoryNameInput.setText("Raktáram")
+
+                    nameInventoryDialog.setView(inventoryNameInput)
+
+                    nameInventoryDialog.setPositiveButton("OK") { dialogInterface, i ->
+                        val inventoryName = inventoryNameInput.text.toString()
+
+                        if (inventoryName.isEmpty()) {
+                            Toast.makeText(
+                                this@MainActivity,
+                                "Nem lehet üres a raktár neve!",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                            nameInventoryDialog.show()
+                        } else {
+
+                            try {
+                                val requestQueue = Volley.newRequestQueue(this)
+                                val URL = "http://$currentServerIP:$currentPort/inventory/set_name"
+                                val jsonBody = JSONObject()
+                                jsonBody.put("inventory_name", inventoryName)
+
+                                val requestBody = jsonBody.toString()
+                                val stringRequest: StringRequest = object : StringRequest(
+                                    Method.POST, URL,
+                                    Response.Listener { response ->
+                                        Log.i("VOLLEY", response!!)
+                                        tvInventoryName.text = "Raktár neve: "+ inventoryName
+                                        Toast.makeText(
+                                            this@MainActivity,
+                                            "Sikeres névadás!",
+                                            Toast.LENGTH_SHORT
+                                        ).show() },
+                                    Response.ErrorListener { error ->
+                                        Log.e(
+                                            "VOLLEY",
+                                            error.toString()
+                                        )
+                                    }) {
+                                    override fun getBodyContentType(): String {
+                                        return "application/json; charset=utf-8"
+                                    }
+
+                                    @Throws(AuthFailureError::class)
+                                    override fun getBody(): ByteArray {
+                                        return try {
+                                            if (requestBody == null) null else requestBody.toByteArray(
+                                                charset("utf-8")
+                                            )
+                                        } catch (uee: UnsupportedEncodingException) {
+                                            VolleyLog.wtf(
+                                                "Unsupported Encoding while trying to get the bytes of %s using %s",
+                                                requestBody,
+                                                "utf-8"
+                                            )
+                                            null
+                                        }!!
+                                    }
+
+                                    override fun parseNetworkResponse(response: NetworkResponse): Response<String> {
+                                        var responseString = ""
+                                        if (response != null) {
+                                            responseString = response.statusCode.toString()
+                                            // can get more details such as response.headers
+                                        }
+                                        return Response.success(
+                                            responseString,
+                                            HttpHeaderParser.parseCacheHeaders(response)
+                                        )
+                                    }
+                                }
+                                requestQueue.add(stringRequest)
+                            } catch (e: JSONException) {
+                                e.printStackTrace()
+                            }
+                        }
+
+                    }
+
+                    nameInventoryDialog.show()
+                }
+            },
+            { error -> })
+
+
+        queue.add(stringRequest)
+
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        val intentResult: IntentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val intentResult: IntentResult = IntentIntegrator.parseActivityResult(
+            requestCode,
+            resultCode,
+            data
+        )
         if(intentResult.contents != null){
 
             when(lastPressedButton){
                 "insertButton" -> {
                     val intent = Intent(this, InsertItemActivity::class.java).apply {
-                        putExtra("itemCode", intentResult.contents )
+                        putExtra("itemCode", intentResult.contents)
                     }
                     startActivity(intent)
                 }
                 "takeOutButton" -> {
                     val intent = Intent(this, TakeOutItemActivity::class.java).apply {
-                        putExtra("itemCode", intentResult.contents )
+                        putExtra("itemCode", intentResult.contents)
                     }
                     startActivity(intent)
                 }
                 "checkButton" -> {
                     val intent = Intent(this, CheckItemActivity::class.java).apply {
-                        putExtra("itemCode", intentResult.contents )
+                        putExtra("itemCode", intentResult.contents)
                     }
                     startActivity(intent)
                 }
@@ -133,7 +246,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             }
             R.id.nav_categories -> {
                 val intent = Intent(this, ViewCategoriesActivity::class.java).apply {
-                    putExtra("category_code", "" )
+                    putExtra("category_code", "")
                     putExtra("category_path", "")
                 }
                 startActivity(intent)
